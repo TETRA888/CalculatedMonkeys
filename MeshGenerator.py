a483b937cac1fd7194b06785d09b7572d243fe13
Asad <tetragamedevelopment@gmail.com>
2024-08-21 18:26:07 -0700

    Add corrected mesh generation

--- MeshGenerator.py
+++ MeshGenerator.py
@@ -4,7 +4,7 @@ import bpy
 # Include math libs
 import math
 
-# Include randomness for funr
+# Include randomness for fun
 import random
 
 # Easier mesh manipulation and selection
@@ -23,34 +23,41 @@ import bmesh
 """                                                                                                                
 
 def create_vr_headset_base():
-    """"""
+    """ This creates a primitve base mesh for the VR headset """
     bpy.ops.mesh.primitive_cube_add(enter_editmode = True, location=(0, 0, 0), scale=(1, 0.25, 0.5))
-
     bpy.ops.mesh.bevel(offset=0.1, offset_pct=0, segments=2, affect='EDGES')
-
     bpy.ops.object.mode_set(mode='OBJECT')
-    
     base_piece = bpy.context.active_object
     
-    return base_piece
+    return base_piece # Returns the reference to the VR headset base mesh object
     
 
 def create_vr_headset_side_pieces():
+    """ This creates the side pieces of the VR headset.
+        In this instance I chose to create two seperate pieces 
+        instead of using an array modifier or object instantiation 
+        so that there is finer control on both of the pieces
+    """
+    # Piece one
     bpy.ops.mesh.primitive_cube_add(enter_editmode = True, location=(-1.05741, 0.549105, 0.140719), scale=(0.1, 0.625, 0.1))
     bpy.ops.mesh.bevel(offset=0.04, offset_pct=0, segments=2, affect='EDGES')
     bpy.ops.object.mode_set(mode='OBJECT')
     
     piece_one = bpy.context.active_object
     
+    # Piece Two
     bpy.ops.mesh.primitive_cube_add(enter_editmode = True, location=(1.05741, 0.549105, 0.140719), scale=(0.1, 0.625, 0.1))
     bpy.ops.mesh.bevel(offset=0.04, offset_pct=0, segments=2, affect='EDGES')
     bpy.ops.object.mode_set(mode='OBJECT')
     
     piece_two = bpy.context.active_object
     
-    return piece_one, piece_two
+    return piece_one, piece_two # Returns the reference to the VR headset side pieces of the mesh objects
     
 def create_vr_headset():
+    """ This simply calls both of the functions to build the entire VR headset
+        and it also appends the same color to all of the seperate objects that make up the VR headset
+    """
     base_piece = create_vr_headset_base()
     piece_one, piece_two = create_vr_headset_side_pieces()
     headset_material = bpy.data.materials.new(name = "Grey")
@@ -59,12 +66,25 @@ def create_vr_headset():
     piece_one.data.materials.append(headset_material)
     piece_two.data.materials.append(headset_material)
     
+    bpy.ops.object.select_all(action='DESELECT')
     
+    piece_one.select_set(True)
+    piece_two.select_set(True)
+    base_piece.select_set(True)
+    
+    bpy.ops.object.join()
+    
+    vr_headset = bpy.context.active_object
+    
+    return vr_headset
+    # Returns joined pieces for easier referencing
 
 def create_monkey():
+    """ Creates the iconic Blender Suzanne monkey """
     bpy.ops.mesh.primitive_monkey_add(location = (0, 0.9, 0))
     monkey = bpy.context.active_object
-    return monkey
+    
+    return monkey # returns reference to th monkey mesh object
 
 def create_material():
     material_instance = bpy.data.materials.new(name = "Random_material")
@@ -136,13 +156,34 @@ def assign_colors(monkey, CLOD):
         face.select = False
     bpy.ops.object.mode_set(mode = 'OBJECT')
 
-def main():
+def create_vr_monkey():
     monkey = create_monkey()
     CLOD = 5
+    
     for i in range(0,CLOD):
         material = create_material()
         monkey.data.materials.append(material)
     assign_colors(monkey, CLOD)
-    create_vr_headset()
+    vr_headset = create_vr_headset()
     
-main()
+    bpy.ops.object.select_all(action='DESELECT')
+    monkey.select_set(True)
+    vr_headset.select_set(True)
+    bpy.ops.object.join()
+    bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='MEDIAN')
+    bpy.ops.object.transform_apply(location=True, rotation=False, scale=False)
+
+    
+    return bpy.context.active_object
+
+steps = 8
+radius = 5
+
+angle_increment = math.tau/steps
+current_angle = 0
+
+for i in range(0,steps):
+    current_monkey = create_vr_monkey()
+    current_angle = angle_increment*i
+    current_monkey.location = (math.cos(current_angle)*radius, math.sin(current_angle)*radius, 0)
+    current_monkey.rotation_euler = (0, 0, current_angle + math.pi/2)
